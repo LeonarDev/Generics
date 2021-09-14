228cea2bb62a4162344f1c8a84378bb778159555
LeonarDev <leonardo.majevski@gmail.com>
2021-09-14 20:20:10 -0300

    generics

--- ProblemaMotivador2/Program.cs
+++ ProblemaMotivador2/Program.cs
@@ -6,7 +6,22 @@ namespace ProblemaMotivador2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintServiceString printService = new PrintServiceString();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{n} values...");
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"Enter value {i}: ");
+                string x = Console.ReadLine();
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+
+            Console.WriteLine($"First: {printService.First()}");
         }
     }
 }
--- SolucaoComGenerics/Program.cs
+++ SolucaoComGenerics/Program.cs
@@ -6,7 +6,22 @@ namespace SolucaoComGenerics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{n} values...");
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"Enter value {i}: ");
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+
+            Console.WriteLine($"First: {printService.First()}");
         }
     }
 }
