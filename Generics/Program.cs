155bd806f602de727d1795f7f33db430041d3f81
LeonarDev <leonardo.majevski@gmail.com>
2021-09-14 21:05:54 -0300

    restrições para generics, GetHashCode e Equals

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Restricoes.Entities;
+using Restricoes.Services;
 
 namespace Generics
 {
@@ -6,7 +10,24 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(vect[0], price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product p = calculationService.Max(list); // <Product> is optional
+
+            Console.WriteLine("Most expensive:");
+            Console.WriteLine(p);
         }
     }
 }
---
+++ GetHashCodeEqualsPersonalizados/Program.cs
@@ -0,0 +1,21 @@
+﻿using GetHashCodeEqualsPersonalizados.Entities;
+using System;
+
+namespace GetHashCodeEqualsPersonalizados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@email.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@email.com" };
+            Client c = new Client { Name = "Copy", Email = "alex@email.com" };
+
+            Console.WriteLine(a.Equals(b)); // False
+            Console.WriteLine(a == b); // False
+            Console.WriteLine(b == c); // False (even with the "email" content being the same, the memory reference is different)
+            Console.WriteLine(a.GetHashCode()); // -137047242
+            Console.WriteLine(b.GetHashCode()); // 685467384
+        }
+    }
+}
